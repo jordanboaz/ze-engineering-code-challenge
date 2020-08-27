13f1d5f07c33d3a378225c363567fd80ab43ed80
Jordan <jordanboazr@gmail.com>
2020-08-27 00:10:08 -0300

    Cleaning up services

--- src/services/pocService.ts
+++ src/services/pocService.ts
@@ -7,32 +7,13 @@ const getPoc = gql`
       products(categoryId: $categoryId, search: $search) {
         id
         title
-        rgb
         images {
           url
         }
         productVariants {
-          availableDate
-          productVariantId
           price
-          inventoryItemId
-          shortDescription
           title
-          published
-          volume
-          volumeUnit
-          description
           subtitle
-          components {
-            id
-            productVariantId
-            productVariant {
-              id
-              title
-              description
-              shortDescription
-            }
-          }
         }
       }
     }
