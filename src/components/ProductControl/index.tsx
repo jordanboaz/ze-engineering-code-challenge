251f41120cfa6e0f070858a7c0136ec724eeb4fb
Jordan <jordanboazr@gmail.com>
2020-08-28 00:00:59 -0300

    Fix keys on ProductSelector

--- src/components/ProductControl/index.tsx
+++ src/components/ProductControl/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   ControlsContainer,
@@ -9,73 +9,80 @@ import {
   Unit,
   UnitText,
   SubmitButton,
-  SubmitText
-
+  SubmitText,
 } from './style';
 import { Props } from './types';
-import theme from '../../theme'
-
+import theme from '../../theme';
 
 const ProductControl = (props: Props) => {
   const [value, setValue] = useState(1);
-  const [units, setUnits] = useState([1, ...(props.units?.length ? props.units : [])]);
+  const [units, setUnits] = useState([
+    1,
+    ...(props.units?.length ? props.units : []),
+  ]);
   const [unitSelected, setUnitedSelect] = useState(0);
 
-  const onChangeValue = (input: string) => { 
+  const onChangeValue = (input: string) => {
     setValue(Number(input));
-  }
+  };
 
   const renderControls = (key: number) => {
     return (
-<ControlsContainer key={key}>
-      <ControlButton onPress={onDecrease}>
-        <ControlLabel>
-          -
-        </ControlLabel>
-        
-      </ControlButton>
-      <ControlValue value={String(value)} onChangeText={onChangeValue} keyboardType="numeric" />
-      <ControlButton onPress={onIncrease}>
-      <ControlLabel style={{color:  theme.colors.yellow.zero}}>
-          +
-        </ControlLabel>
-      </ControlButton>
-    </ControlsContainer>
-    )
-  }
+      <ControlsContainer key={key}>
+        <ControlButton onPress={onDecrease}>
+          <ControlLabel>-</ControlLabel>
+        </ControlButton>
+        <ControlValue
+          value={String(value)}
+          onChangeText={onChangeValue}
+          keyboardType="numeric"
+        />
+        <ControlButton onPress={onIncrease}>
+          <ControlLabel style={{ color: theme.colors.yellow.zero }}>
+            +
+          </ControlLabel>
+        </ControlButton>
+      </ControlsContainer>
+    );
+  };
 
   const selectUnit = (eachUnit: number, index: number) => {
     setUnitedSelect(index);
     setValue(eachUnit);
-  }
+  };
 
   const onIncrease = () => {
-    setValue(value => value + 1);
-  }
+    setValue((value) => value + 1);
+  };
 
   const onDecrease = () => {
-    if(value > 1){
-      setValue(value => value -1);
+    if (value > 1) {
+      setValue((value) => value - 1);
     }
-  }
+  };
 
-  return <Container>
-    <UnitsControlContainer>
-      {units.map((eachUnit: number, index: number) => {
-        if(unitSelected === index){
-          return renderControls(index);
-        }
-        return <Unit key={eachUnit} onPress={() => selectUnit(eachUnit, index)}>
-          <UnitText>{eachUnit}</UnitText>
-          </Unit>
-      })}
-    </UnitsControlContainer>
+  return (
+    <Container>
+      <UnitsControlContainer>
+        {units.map((eachUnit: number, index: number) => {
+          if (unitSelected === index) {
+            return renderControls(index);
+          }
+          return (
+            <Unit key={index} onPress={() => selectUnit(eachUnit, index)}>
+              <UnitText>{eachUnit}</UnitText>
+            </Unit>
+          );
+        })}
+      </UnitsControlContainer>
       <SubmitButton onPress={() => props.onSubmit(value)}>
-    <SubmitText>{`Adicionar (${value})`}</SubmitText>
-    <SubmitText>{props.price ? `R$ ${(props.price * value).toFixed(2)}` : ''}</SubmitText>
+        <SubmitText>{`Adicionar (${value})`}</SubmitText>
+        <SubmitText>
+          {props.price ? `R$ ${(props.price * value).toFixed(2)}` : ''}
+        </SubmitText>
       </SubmitButton>
-  </Container>
-
-}
+    </Container>
+  );
+};
 
 export default ProductControl;
